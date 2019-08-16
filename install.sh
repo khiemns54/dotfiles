c671e71bac3dd496ceb49e25da6fdb99bfdaf6d2
khiemNS <khiemns.k54@gmail.com>
2019-08-16 09:46:44 +0900

    dev

--- install.sh
+++ install.sh
@@ -2,28 +2,29 @@ export DOTFILES=$(pwd)
 sudo visudo
 brew install git
 brew install mysql@5.7
-brew install redis 
+brew install redis
 brew install unrar
 brew install pkg-config
 brew install imagemagick
-brew install postgresql 
+brew install postgresql
 brew install ag
 brew install zsh
 brew install zlib
-brew install tig 
-brew install mecab 
+brew install tig
+brew install mecab
 
 brew install mecab mecab-ipdic
-brew install mecab-ipadic 
+brew install mecab-ipadic
 brew install ctags
-brew install pythonx 
+brew install pythonx
 brew install greenlet
-brew install macvim 
+brew install macvim
 brew install tmux
-brew install vtop 
-brew install tpm 
+brew install vtop
+brew install tpm
 brew install tree
-brew install tmux 
+brew install tmux
+brew install gpg
 
 git submodule update --init --recursive
 
